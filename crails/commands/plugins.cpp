9f70ed7f665736b39a348980dd9b7c1228a4d037
plaristote <michael@unetresgrossebite.com>
2022-08-25 17:10:31 +0200

    adds missing comet command in PluginManager

--- crails/commands/plugins.cpp
+++ crails/commands/plugins.cpp
@@ -1,9 +1,11 @@
 #include "plugins.hpp"
 #include "../plugins/odb/module.hpp"
+#include "../plugins/comet/plugin.hpp"
 
 using namespace std;
 
 PluginManager::PluginManager()
 {
   add_command("odb", []() { return make_shared<OdbModule>(); });
+  add_command("comet", []() { return make_shared<CometPlugin>(); });
 }
