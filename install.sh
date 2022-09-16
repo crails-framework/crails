ee269d5597c92e5b14d405f0e6b241cf52113a4b
plaristote <michael@unetresgrossebite.com>
2022-09-16 23:10:11 +0200

    update install script

--- install.sh
+++ install.sh
@@ -14,52 +14,52 @@ echo " ╚██████╗██║  ██║██║  ██║██║
 echo "  ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝╚══════╝"
 echo ""
 
-if [[ -z "$CRAILS_VERSION" ]] ; then
+if [ -z "$CRAILS_VERSION" ] ; then
   echo -n "> Which version of crails would you like to install (default: $DEFAULT_CRAILS_VERSION): "
   read CRAILS_VERSION
-  if [[ -z "$CRAILS_VERSION" ]] ; then CRAILS_VERSION="$DEFAULT_CRAILS_VERSION" ; fi
+  if [ -z "$CRAILS_VERSION" ] ; then CRAILS_VERSION="$DEFAULT_CRAILS_VERSION" ; fi
 fi
 
-if [[ -z "$COMPILER" ]] ; then
+if [ -z "$COMPILER" ] ; then
   echo -n "> Which compiler do you wish to use (default: $DEFAULT_COMPILER): "
   read COMPILER
-  if [[ -z "$COMPILER" ]] ; then COMPILER="$DEFAULT_COMPILER" ; fi
+  if [ -z "$COMPILER" ] ; then COMPILER="$DEFAULT_COMPILER" ; fi
 fi
 
-if [[ -z "$sql_backends" ]] ; then
+if [ -z "$sql_backends" ] ; then
   echo "(i) Available database backends are: sqlite pgsql mysql oracle"
   echo -n "> Which database backend(s) do you want to use (separated with spaces): "
   read sql_backends
 fi
 
-if [[ ! -z "$sql_backends" ]] ; then
-  if [[ -z "$WITH_ODB_COMPILER" ]] ; then
+if [ ! -z "$sql_backends" ] ; then
+  if [ -z "$WITH_ODB_COMPILER" ] && ! which odb > /dev/null ; then
     echo "(i) The ODB compiler is needed for SQL support. This installer will build ODB unless told otherwise."
-    if [[ ! `find /usr/lib/gcc -name gcc-plugin.h | grep gcc-plugin.h` ]] ; then
+    if ! find /usr/lib/gcc -name gcc-plugin.h | grep gcc-plugin.h > /dev/null ; then
       echo '(!) gcc-plugin.h was not found. If you want to build the ODB compiler, make sure you have the gcc-plugin development package installed in your system.'
     else
       echo '(i) gcc-plugin.h found, the ODB compiler should build successfully.'
     fi
     echo -n "> Build the ODB compiler (y/n): "
     read WITH_ODB_COMPILER
-    if [[ -z "$WITH_ODB_COMPILER" ]] ; then WITH_ODB_COMPILER="y" ; fi
+    if [ -z "$WITH_ODB_COMPILER" ] ; then WITH_ODB_COMPILER="y" ; fi
   fi
 fi
 
-if [[ -z "$WITH_COMET" ]] ; then
+if [ -z "$WITH_COMET" ] ; then
   echo -n "> Build comet.cpp (https://github.com/crails-framework/comet.cpp) (y/n): "
   read WITH_COMET
 fi
 
-if [[ -z "$use_system_libraries" ]] ; then
+if [ -z "$use_system_libraries" ] ; then
   echo -n "> Should we use system libraries when available (y/n): "
   read use_system_libraries
-  if [[ -z "$use_system_libraries" ]] ; then use_system_libraries="n" ; fi
+  if [ -z "$use_system_libraries" ] ; then use_system_libraries="n" ; fi
 fi
 
 echo -n "> Install path (default: $DEFAULT_INSTALL_ROOT): "
 read INSTALL_ROOT
-if [[ -z "$INSTALL_ROOT" ]] ; then INSTALL_ROOT="$DEFAULT_INSTALL_ROOT" ; fi
+if [ -z "$INSTALL_ROOT" ] ; then INSTALL_ROOT="$DEFAULT_INSTALL_ROOT" ; fi
 
 COMPILER_VERSION=`$COMPILER --version | sed -n 1p | awk '{print $3}' | cut -d. -f1`
 BUILD_DIR="build-$COMPILER-$COMPILER_VERSION"
@@ -79,6 +79,8 @@ crails_packages=(
   libcrails-html-views
   libcrails-databases
   libcrails-http-client
+  libcrails-mail
+  libcrails-signin
   libcrails-xmlrpc
   libcrails-proxy
   libcrails-tests
@@ -86,9 +88,10 @@ crails_packages=(
 
 system_packages=()
 
-if [[ "$use_system_libraries" == "y" ]] ; then
+if [ "$use_system_libraries" = "y" ] ; then
   system_packages+=(
     ?sys:libboost-any
+    ?sys:libboost-asio
     ?sys:libboost-beast
     ?sys:libboost-config
     ?sys:libboost-filesystem
@@ -117,7 +120,7 @@ if [[ "$use_system_libraries" == "y" ]] ; then
   echo "+ Using system packages: ${system_packages[@]}"
 fi
 
-if [[ ! -z "$sql_backends" ]] ; then
+if [ ! -z "$sql_backends" ] ; then
   crails_packages+=(libcrails-odb)
 fi
 
@@ -126,7 +129,7 @@ fi
 ##
 echo "+ creating package at $BUILD_DIR"
 
-if [[ -d "$BUILD_DIR" ]] ; then
+if [ -d "$BUILD_DIR" ] ; then
   echo "++ removing previously existing package at $BUILD_DIR"
   rm -rf "$BUILD_DIR"
 fi
@@ -149,7 +152,7 @@ echo "+ building core components"
 bpkg build crails    --yes ${system_packages[@]}
 bpkg build libcrails --yes ${system_packages[@]}
 
-if [[ "$WITH_COMET" == "y" ]] ; then
+if [ "$WITH_COMET" = "y" ] ; then
   bpkg build comet   --yes ${system_packages[@]}
 fi
 
@@ -176,18 +179,21 @@ for package in ${crails_packages[@]} ; do
   bpkg build $package --yes ${system_packages[@]}
 done
 
-if [[ -z "$install_confirmed" ]] ; then
+if [ -z "$install_confirmed" ] ; then
   echo -n "> Install to $INSTALL_ROOT (y/n): "
   read install_confirmed
 fi
   
-if [[ "$install_confirmed" == "y" ]] ; then
+if [ "$install_confirmed" = "y" ] ; then
   bpkg install --all --recursive \
     config.install.root="$INSTALL_ROOT" \
     config.install.sudo=sudo
 fi
 
-if [[ "$WITH_ODB_COMPILER" == "y" ]] ; then
+##
+## Build ODB compiler
+##
+if [ "$WITH_ODB_COMPILER" = "y" ] ; then
   cd ..
   bpkg create -d odb-gcc cc \
     config.cxx=g++ \
@@ -200,7 +206,10 @@ if [[ "$WITH_ODB_COMPILER" == "y" ]] ; then
   bpkg install odb
 fi
 
-if [[ "$WITH_COMET" == "y" ]] ; then
+##
+## Build Comet.cpp
+##
+if [ "$WITH_COMET" = "y" ] ; then
   cd ..
   bash <(curl -s "https://raw.githubusercontent.com/crails-framework/comet.cpp/master/install.sh")
 fi
